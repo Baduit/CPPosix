862b315f363834befe3d0aacc51aa3488a4eac17
Simon Bertho <simon.bertho@epitech.eu>
2019-01-11 20:02:27 +0100

    Synxtax found for the "pseudo concepts" and Writtable concept done.

--- src/Concept.hpp
+++ src/Concept.hpp
@@ -1,6 +1,55 @@
 #pragma once
 
+#include <type_traits>
+
 namespace Cpposix
 {
+/*
+	HELPER FUNCTIONS
+*/
+	
+template <typename T>
+inline constexpr bool isWrittable()
+{
+	return (std::has_unique_object_representations<T>::value) && (!std::is_pointer<T>::value);
+}
+
+template <typename T>
+inline constexpr bool isWrittable(const T& t) { return isWrittable<T>(); }
+
+
+template <typename T>
+inline constexpr bool isSmartPointer();
+
+template <typename T>
+inline constexpr bool isWrittabeSmartPointer();
+
+
+/*
+CONCEPTS
+
+Writtable => unique_representatoin && not a pointer
+	example:
+		template<typename Writtable>
+		void test(Writtable writtable);
+
+SmartPointer
+WrittableSmartPointer
+
+ContiguousContainer pas sur que ça soit utile
+WrittableContainer pas sur que ça soit utile
+
+
+ForkCallback
+
+*/
+
+template <typename T>
+using IsWrittable = std::enable_if_t<isWrittable<T>()>;
+
+
+// used for better error message with static_assert
+template <typename T>
+using IsUnwrittable = std::enable_if_t<!isWrittable<T>()/*ajouter ici les autres négations de concepts plus tard*/>;
 
 }
--- test/test_concept.cpp
+++ test/test_concept.cpp
@@ -1,10 +1,66 @@
 #include <cassert>
+#include <iostream>
+#include <cstdint>
 
 #include "Concept.hpp"
 
 using namespace Cpposix;
 
+
+#pragma pack(1)
+struct packed
+{
+	int a;
+	int b;
+	uint8_t c;
+};
+#pragma pack()
+
+struct not_packed
+{
+	int a;
+	int b;
+	uint8_t c;
+};
+
+enum class OverloadUsed
+{
+	WRITTABLE,
+	WRITTABLE_SMART_POINTER,
+	WRITTABLE_CONTAINER,
+	NONE
+};
+
+template <typename Writtable, typename = IsWrittable<Writtable>>
+OverloadUsed my_function(Writtable writtable)
+{
+	return OverloadUsed::WRITTABLE;
+}
+
+/* template <typename T, typename = IsUnwrittable<T>>
+OverloadUsed my_function(T t)
+{
+	return OverloadUsed::NONE;
+} */
+
+void testIsWrittable()
+{
+	assert(Cpposix::isWrittable<int>());
+	assert(!Cpposix::isWrittable<double>());
+	assert(Cpposix::isWrittable<packed>());
+	assert(!Cpposix::isWrittable<not_packed>());
+	assert(!Cpposix::isWrittable<void*>());
+
+	assert(Cpposix::isWrittable(5));
+	assert(!Cpposix::isWrittable(5.5));
+	assert(Cpposix::isWrittable(packed()));
+	assert(!Cpposix::isWrittable(not_packed()));
+	assert(!Cpposix::isWrittable(testIsWrittable));
+
+	assert(my_function(7) == OverloadUsed::WRITTABLE);
+}
+
 int main()
 {
-	assert(true);
+	testIsWrittable();
 }
