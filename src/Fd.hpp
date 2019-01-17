83012f689cb792e6d53b15e8aeb61870caacc4ad
Simon Bertho <simon.bertho@epitech.eu>
2019-01-18 00:16:29 +0100

    Fd: write functions, begin read functions

--- src/Fd.hpp
+++ src/Fd.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <unistd.h>
+#include <cstddef>
+
+#include "Concept.hpp"
+#include "Expected.hpp"
 
 namespace Cpposix
 {
@@ -9,11 +13,11 @@ class Fd
 {
 	public:
 		Fd() = default;
-		Fd(int fd): _fd(fd) {}
+		explicit Fd(int fd): _fd(fd) {}
 
 		virtual ~Fd() { close(); }
 
-		operator bool () const { return fd != -1; }
+		operator bool () const { return _fd >= 0; }
 
 		void assign(int fd) { _fd = fd; }
 		void close()
@@ -22,6 +26,93 @@ class Fd
 				::close(_fd);
 		}
 
+		template <typename T>
+		Expected<std::size_t> write(const T& t, std::size_t size)
+		{
+			if constexpr (std::is_pointer<T>() || std::is_array<T>())
+				return writePointer(t, size);
+			else if constexpr (isWritable<T>())
+				return writeWritable(t, size);
+			else if constexpr (isWritableContainer<T>())
+				return writeContainer(t, size);
+			else
+			{
+				static_assert(isWritable<T>() || std::is_pointer<T>() || std::is_array<T>() || isWritableContainer<T>(), "You are trying to write something you should not try to write.");
+				return {};
+			}
+		}
+
+		template <typename T>
+		Expected<std::size_t> write(const T& t)
+		{
+			if constexpr (isWritable<T>())
+				return writeWritable(t);
+			else if constexpr (isWritableContainer<T>())
+				return writeContainer(t);
+			else
+			{
+				static_assert(isWritable<T>() || isWritableContainer<T>(), "You are trying to write something you should not try to write.");
+				return {};
+			}
+		}
+
+		template <typename T>
+		Fd& operator<<(const T& t) { write(t); return *this; }
+
+		
+		
+		// read
+
+		// oparator >>
+
+	private:
+		template <typename Writable, typename = IsWritable<Writable>>
+		Expected<std::size_t>	writePointer(const Writable* writable_ptr, std::size_t size)
+		{
+			if (*this)
+			{
+				auto result = ::write(_fd, writable_ptr, size);
+				if (result != -1)
+					return static_cast<std::size_t>(result);
+				else
+					return Error();
+			}
+			else
+				return Error(EBADF);
+		}
+
+		template <typename Readable, typename = IsReadable<Readable>>
+		Expected<std::size_t>	readPointer(const Readable* readable_ptr, std::size_t size)
+		{
+			if (*this)
+			{
+					return Error();
+			}
+			else
+				return Error(EBADF);
+		}
+
+		/*
+		** Conveniance methods
+		*/
+
+		template <typename Writable, typename = IsWritable<Writable>>
+		Expected<std::size_t>	writeWritable(Writable writable, std::size_t size = sizeof(Writable)) { return writePointer(&writable, size); }
+
+		template <typename WritableContainer, typename = IsWritableContainer<WritableContainer>>
+		Expected<std::size_t> writeContainer(WritableContainer writableContainer) { return writePointer(writableContainer.data(), writableContainer.size()); }
+
+		template <typename WritableContainer, typename = IsWritableContainer<WritableContainer>>
+		Expected<std::size_t> writeContainer(WritableContainer writableContainer, std::size_t size) { return writePointer(writableContainer.data(), size); }
+
+		template <typename Readable, typename = IsReadable<Readable>>
+		Expected<std::size_t>	readReadable(Readable readable, std::size_t size = sizeof(Readable)) { return readPointer(&readable, size); }
+
+		template <typename ReadableContainer, typename = IsReadableContainer<ReadableContainer>>
+		Expected<std::size_t> readContainer(ReadableContainer readableContainer) { return readPointer(readableContainer.data(), readableContainer.size()); }
+
+		template <typename ReadableContainer, typename = IsReadableContainer<ReadableContainer>>
+		Expected<std::size_t> readContainer(ReadableContainer readableContainer, std::size_t size) { return readPointer(readableContainer.data(), size); }
 
 	protected:
 		int _fd = -1;
--- test/test_fd.cpp
+++ test/test_fd.cpp
@@ -5,9 +5,33 @@
 #include <vector>
 #include <list>
 
-#include "Buffer.hpp"
+#include "Fd.hpp"
+
+using namespace Cpposix;
+using namespace std::string_literals;
+
+void test_write()
+{
+	Fd wrong_fd(-4);
+	assert(!wrong_fd);
+	assert(!wrong_fd.write(5));
+
+	Fd fd(1);
+	assert(fd);
+	assert(fd.write(static_cast<uint8_t>('I')).get_or(0) == sizeof(uint8_t));
+
+	std::string str = "\nContainer";
+	assert(fd.write(str).get_or(0) == str.size());
+
+	std::string str2 = "\nContainerblabla";
+	assert(fd.write(str2, str.size()).get_or(0) == str.size());
+
+	assert(fd.write("\n", 1).get_or(0) == 1);
+
+	assert((fd << "Operator <<\n"s));
+}
 
 int main()
 {
-
+	test_write();
 }
