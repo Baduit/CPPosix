0756887f4149f454f20d838e822c5bd77818e6fd
Simon Bertho <simon.bertho@epitech.eu>
2019-02-09 21:15:16 +0100

    env test

--- test/test_env.cpp
+++ test/test_env.cpp
@@ -7,7 +7,36 @@
 
 #include "Env.hpp"
 
+using namespace Cpposix;
+
 int main()
 {
+	{
+		assert(setEnv("yolol", "plop"));
+		auto yolol_env = getEnv("yolol");
+		assert(yolol_env);
+		assert(yolol_env.get() == "plop");
+	}
 
+	{
+		assert(setEnv("yolol", "salut"));
+		auto yolol_env = getEnv("yolol");
+		assert(yolol_env);
+		assert(yolol_env.get() == "salut");
+	}
+
+	{
+		assert(setEnv("yolol", "kkkk", false));
+		auto yolol_env = getEnv("yolol");
+		assert(yolol_env);
+		assert(yolol_env.get() == "salut");
+	}
+
+	// this test can fail if someone already named a var env zefzefzefzefzefzef bur seriouslu who would do that???
+	{
+		assert(setEnv("zefzefzefzefzefzef", "kkkk", false));
+		auto yolol_env = getEnv("zefzefzefzefzefzef");
+		assert(yolol_env);
+		assert(yolol_env.get() == "kkkk");
+	}
 }
