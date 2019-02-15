2ee00a7f2e4a4705b76132c7e239fd99a513f6e8
Simon Bertho <simon.bertho@epitech.eu>
2019-02-15 11:27:05 +0100

    process done, i just need to test the communications

--- src/Process.hpp
+++ src/Process.hpp
@@ -1,7 +1,70 @@
 #pragma once
 
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#include "Expected.hpp"
+#include "PipeFd.hpp"
+
 namespace Cpposix
 {
-// do not forget nice(int)
+
+
+class ChildProcess
+{
+	public:
+		// template
+		template <typename Cb, class... Args>
+		ChildProcess(Cb&& cb, Args&&... args)
+		{
+			_pid = fork();
+			if (_pid == -1)
+				throw CpposixException("ChildProcess: Error while forking.");
+
+			if (_pid == 0)
+			{
+				if constexpr (std::is_same<int, typename std::result_of<Cb(Args&&...)>::type>::value)
+				{
+					exit(cb(std::forward<Args...>(args)...));
+				}
+				else
+				{
+					cb(std::forward<Args...>(args)...);
+					exit(EXIT_SUCCESS);
+				}
+			}
+		}
+
+		Expected<Void>	kill()
+		{
+			if (_pid == 0)
+				return Error();
+
+			if (::kill(_pid, SIGKILL) == -1)
+				return Error();
+
+			return Void();
+		}
+
+		Expected<int>	wait(int options = 0)
+		{
+			if (_pid == 0)
+				return Error();
+			
+			int	status = 0;
+			if (::waitpid(_pid, &status, options) == -1)
+				return Error();
+			return WEXITSTATUS(status);
+		}
+
+		pid_t	getChildPid() const { return _pid; }
+	public:
+		Pipe	communication;
+	
+	private:
+		pid_t	_pid;
+};
+
 // for exec functions, do not forget to try to add a end of scope guard, because exec family functions seems to break RAII
 }
--- test/test_process.cpp
+++ test/test_process.cpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector>
 #include <list>
+#include <thread>
+#include <chrono>
 
 #include "Process.hpp"
 
@@ -11,5 +13,45 @@ using namespace Cpposix;
 
 int main()
 {
+	{
+		auto cb =
+			[]()
+			{
+				std::cout << "It is working! "<< std::endl;
+				std::this_thread::sleep_for(std::chrono::milliseconds(100));
+				return 5;
+			};
 
+		ChildProcess p(cb);
+		auto result = p.wait();
+		assert(result);
+		assert(result.getOr(77) == 5);
+	}
+
+	{
+		auto cb =
+			[]()
+			{
+				std::cout << "It is working! "<< std::endl;
+				std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			};
+
+		ChildProcess p(cb);
+		auto result = p.wait();
+		assert(result);
+		assert(result.getOr(77) == 0);
+	}
+
+	{
+		auto cb =
+			[]()
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(200));
+				std::cout << "It is not working! :'("<< std::endl;
+			};
+
+		ChildProcess p(cb);
+		assert(p.kill());
+		assert(p.wait());
+	}
 }
